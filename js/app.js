16f4cb8f45f4fca75dbfef96e771299623c15d2b
Andy Hamilton <andyhmltn@gmail.com>
2013-09-10 13:35:38 +0100

    Added proper commenting to app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 var app = angular.module('PortfolioViewer', []).config(['$routeProvider', function($routeProvider) {
 
+  // This sets up the relevent routing
+  // settings. Currently there is an index
+  // action and a view action
+  
   $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'PortfolioController'})
                 .when('/view/:id', {templateUrl: 'partials/view.html', controller: 'ProjectController'})
                 .otherwise({redirectTo: '/'})
