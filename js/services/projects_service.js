3ed73a67ef8a8e1b0e8adc9a527b3b49b14fbb83
Andy Hamilton <andyhmltn@gmail.com>
2013-10-04 12:56:22 +0100

    Made the project service DRYer

--- js/services/projects_service.js
+++ js/services/projects_service.js
@@ -1,22 +1,24 @@
 app.factory('projectsService', function($http) {
 
+  var url = 'http://localhost:3000/projects/'
+
   return {
     getProjects: function() {
-      return $http.get('http://localhost:3000/projects')
+      return $http.get(url)
                   .then(function(result) {
                     return result.data
                   })
     },
 
     getProject: function(id) {
-      return $http.get('http://localhost:3000/projects/'+id)
+      return $http.get(url+id)
                   .then(function(result) {
                     return result.data
                   })
     },
 
     deleteProject: function(id) {
-      return $http.delete('http://localhost:3000/projects/'+id)
+      return $http.delete(url+id)
                   .then(function(result) {
                     return result.data
                   })
